a348cb5130e0b6d1e504bca759d7837934a43702
jefferson-kkk <jecorrea.2008@gmail.com>
2026-09-19 15:46:51 -0300

    finalização projeto-feira

--- teste_conexao.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-    $pdo = new PDO('mysql:host=localhost;charset=utf8mb4', 'root', 'senaisp');
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "CONEXAO_OK";
-} catch (Throwable $e) {
-    echo 'ERRO: ' . $e->getMessage();
-}
